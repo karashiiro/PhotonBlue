e6900fb8dd969f830ff436bd0273beacd09fb098
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-07-14 02:52:15 -0700

    Implement NIFL text parsing

--- src/PhotonBlue.Tests/NiflFileTests.cs
+++ src/PhotonBlue.Tests/NiflFileTests.cs
@@ -16,4 +16,16 @@ public class NiflFileTests
         Assert.Equal(0x304c4552U, nifl.Rel0.Magic);
         Assert.Equal(0x30464f4eU, nifl.Nof0.Magic);
     }
+    
+    [Fact]
+    public void NiflFile_Parses_Text()
+    {
+        using var data = File.OpenRead(@"..\..\..\..\..\tut_006353.text");
+        var nifl = new NiflFile(data);
+        nifl.LoadFile();
+
+        var text = nifl.ReadText();
+        Assert.NotEmpty(text);
+        Assert.All(text, Assert.NotEmpty);
+    }
 }
