78596c4d6dfb36257fb64463b4201bf7388a1d84
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-07-16 14:54:48 -0700

    Add more failing test cases for ICE extraction

--- src/PhotonBlue.Tests/IceFileTests.cs
+++ src/PhotonBlue.Tests/IceFileTests.cs
@@ -7,14 +7,29 @@ namespace PhotonBlue.Tests;
 public class IceFileTests
 {
     [Fact]
-    public void IceFile_Parses_V4_Kraken_Data()
+    public void IceFile_Parses_V4_Kraken_Data_1()
     {
         using var data = File.OpenRead(@"..\..\..\..\..\testdata\b568a6a6c428485a57b67a1da466de");
         var ice = new IceFileV4(data);
         ice.LoadFile();
         
         Assert.Equal(1, ice.Group1Entries.Count);
+        Assert.All(ice.Group1Entries, AssertEntryValid);
+        
         Assert.Equal(3, ice.Group2Entries.Count);
+        Assert.All(ice.Group2Entries, AssertEntryValid);
+    }
+    
+    [Fact]
+    public void IceFile_Parses_V4_Kraken_Data_2()
+    {
+        using var data = File.OpenRead(@"..\..\..\..\..\testdata\0000064b91444b04df5d95f6a0bc55be");
+        var ice = new IceFileV4(data);
+        ice.LoadFile();
+        
+        Assert.Equal(0, ice.Group1Entries.Count);
+        Assert.Equal(5, ice.Group2Entries.Count);
+        Assert.All(ice.Group2Entries, AssertEntryValid);
     }
     
     [Fact]
@@ -26,10 +41,11 @@ public class IceFileTests
         
         Assert.Equal(0, ice.Group1Entries.Count);
         Assert.Equal(2, ice.Group2Entries.Count);
+        Assert.All(ice.Group2Entries, AssertEntryValid);
     }
     
     [Fact]
-    public void IceFile_Parses_V4_PRS_Data()
+    public void IceFile_Parses_V4_PRS_Data_1()
     {
         using var data = File.OpenRead(@"..\..\..\..\..\testdata\cb1001342c1f786545795140c345f1");
         var ice = new IceFileV4(data);
@@ -37,6 +53,19 @@ public class IceFileTests
         
         Assert.Equal(0, ice.Group1Entries.Count);
         Assert.Equal(31, ice.Group2Entries.Count);
+        Assert.All(ice.Group2Entries, AssertEntryValid);
+    }
+    
+    [Fact]
+    public void IceFile_Parses_V4_PRS_Data_2()
+    {
+        using var data = File.OpenRead(@"..\..\..\..\..\testdata\0000ad8daf393f31da0fd7e26829c819");
+        var ice = new IceFileV4(data);
+        ice.LoadFile();
+        
+        Assert.Equal(0, ice.Group1Entries.Count);
+        Assert.Equal(31, ice.Group2Entries.Count);
+        Assert.All(ice.Group2Entries, AssertEntryValid);
     }
     
     [Fact]
@@ -48,5 +77,12 @@ public class IceFileTests
         
         Assert.Equal(0, ice.Group1Entries.Count);
         Assert.Equal(1, ice.Group2Entries.Count);
+        Assert.All(ice.Group2Entries, AssertEntryValid);
+    }
+
+    private void AssertEntryValid(IceFile.FileEntry entry)
+    {
+        Assert.True(entry.Header.FileNameRaw.Length <= 0x20);
+        Assert.True(entry.Header.DataSize == entry.Data.Length);
     }
 }
