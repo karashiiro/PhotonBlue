d46c0d4934d5f93b8e5ca89b74a8b08c7af7d959
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-07-23 10:13:41 -0700

    Add file handle manager

---
+++ src/PhotonBlue/Data/FileHandleManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Concurrent;
+
+namespace PhotonBlue.Data;
+
+public class FileHandleManager
+{
+    private readonly ConcurrentQueue<(bool, WeakReference<BaseFileHandle>)> _fileQueue;
+
+    internal FileHandleManager()
+    {
+        _fileQueue = new ConcurrentQueue<(bool, WeakReference<BaseFileHandle>)>();
+    }
+
+    /// <summary>
+    /// Creates a new handle to a game file but does not load it. You will need to call <see cref="ProcessQueue"/> or the wrapper function
+    /// <see cref="GameData.ProcessFileHandleQueue"/>  yourself for these handles to be loaded, on a different thread.
+    /// </summary>
+    /// <param name="path">The path to the file to load.</param>
+    /// <param name="loadComplete">Whether or not to load the complete file when processing the queue.</param>
+    /// <typeparam name="T">The type of <see cref="FileResource"/> to load.</typeparam>
+    /// <returns>A handle to the file to be loaded.</returns>
+    public FileHandle<T> CreateHandle<T>(string path, bool loadComplete = true) where T : FileResource, new()
+    {
+        var handle = new FileHandle<T>(path);
+        var weakRef = new WeakReference<BaseFileHandle>(handle);
+        _fileQueue.Enqueue((loadComplete, weakRef));
+
+        return handle;
+    }
+
+    /// <summary>
+    /// Processes enqueued file handles that haven't been loaded yet. You should call this on a different thread to process handles.
+    /// </summary>
+    public void ProcessQueue()
+    {
+        while (HasPendingFileLoads)
+        {
+            var res = _fileQueue.TryDequeue(out var entry);
+            var (loadComplete, weakRef) = entry;
+            if (res && weakRef!.TryGetTarget(out var handle))
+            {
+                if (loadComplete)
+                {
+                    handle.Load();
+                }
+                else
+                {
+                    handle.LoadHeadersOnly();
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Whether the file queue contains any files that are yet to be loaded.
+    /// </summary>
+    public bool HasPendingFileLoads => !_fileQueue.IsEmpty;
+}
--- src/PhotonBlue/GameData.cs
+++ src/PhotonBlue/GameData.cs
@@ -9,6 +9,12 @@ public class GameData
     /// </summary>
     public DirectoryInfo DataPath { get; }
 
+    /// <summary>
+    /// Provides access to the <see cref="FileHandleManager"/> which allows you to create new <see cref="FileHandle{T}"/>s which then allows you to
+    /// easily defer file loading onto another thread.
+    /// </summary>
+    public FileHandleManager FileHandleManager { get; }
+
     public GameData(string pso2BinPath)
     {
         DataPath = new DirectoryInfo(pso2BinPath);
@@ -22,6 +28,8 @@ public class GameData
         {
             throw new ArgumentException("DataPath must point to the pso2_bin directory.", nameof(pso2BinPath));
         }
+
+        FileHandleManager = new FileHandleManager();
     }
 
     /// <summary>
@@ -36,7 +44,7 @@ public class GameData
         file.Load();
         return file.State != BaseFileHandle.FileState.Loaded ? null : file.Value;
     }
-    
+
     /// <summary>
     /// Fetches a file given a data path relative to pso2_bin, parsing only its headers.
     /// </summary>
@@ -49,7 +57,7 @@ public class GameData
         file.LoadHeadersOnly();
         return file.State != BaseFileHandle.FileState.Loaded ? null : file.Value;
     }
-    
+
     /// <summary>
     /// Fetches a file given a filesystem path.
     /// </summary>
@@ -62,7 +70,7 @@ public class GameData
         file.Load();
         return file.State != BaseFileHandle.FileState.Loaded ? null : file.Value;
     }
-    
+
     /// <summary>
     /// Fetches a file given a filesystem path, parsing only its headers.
     /// </summary>
@@ -74,5 +82,25 @@ public class GameData
         var file = new FileHandle<T>(path);
         file.LoadHeadersOnly();
         return file.State != BaseFileHandle.FileState.Loaded ? null : file.Value;
+    }
+
+    /// <summary>
+    /// Creates a new handle to a game file but does not load it. You will need to call <see cref="ProcessFileHandleQueue"/> yourself for these handles
+    /// to be loaded, on a different thread.
+    /// </summary>
+    /// <param name="path">The path to the file to load</param>
+    /// <typeparam name="T">The type of <see cref="FileResource"/> to load</typeparam>
+    /// <returns>A handle to the file to be loaded</returns>
+    public FileHandle<T> GetFileHandle<T>(string path) where T : FileResource, new()
+    {
+        return FileHandleManager.CreateHandle<T>(path);
+    }
+
+    /// <summary>
+    /// Processes enqueued file handles that haven't been loaded yet. Call this on a different thread to process handles.
+    /// </summary>
+    public void ProcessFileHandleQueue()
+    {
+        FileHandleManager.ProcessQueue();
     }
 }
