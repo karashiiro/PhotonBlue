49c3a42f3f90b5498b5e2cb12e605f3002376b86
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-07-28 22:59:45 -0700

    Allow object to be disposed multiple times

--- src/PhotonBlue/MicroDisposable.cs
+++ src/PhotonBlue/MicroDisposable.cs
@@ -5,20 +5,27 @@ namespace PhotonBlue;
 /// This is intended for when creating a specific wrapper type would be overkill.
 /// </summary>
 /// <typeparam name="T">The type of the object to wrap.</typeparam>
-public readonly struct MicroDisposable<T> : IDisposable
+public struct MicroDisposable<T> : IDisposable
 {
     private readonly T disposeArg;
     private readonly Action<T> disposeAction;
+
+    private bool disposed;
 
     private MicroDisposable(T disposeTarget, Action<T> dispose)
     {
         disposeArg = disposeTarget;
         disposeAction = dispose;
+        disposed = false;
     }
         
     public void Dispose()
     {
-        disposeAction(disposeArg);
+        if (!disposed)
+        {
+            disposed = true;
+            disposeAction(disposeArg);
+        }
     }
 
     public static MicroDisposable<T> Create(T disposeTarget, Action<T> dispose)
