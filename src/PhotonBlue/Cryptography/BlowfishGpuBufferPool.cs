d32b4bb4814d54f38efd285e21ebf426ae9459fc
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-07-28 23:57:54 -0700

    Increase GPU pool concurrency to reduce wait times

--- src/PhotonBlue/Cryptography/BlowfishGpuBufferPool.cs
+++ src/PhotonBlue/Cryptography/BlowfishGpuBufferPool.cs
@@ -7,7 +7,7 @@ public sealed class BlowfishGpuBufferPool : IDisposable
 {
     public const int DataBufferSize = 524288;
     
-    private const int MaxConcurrency = 32;
+    private const int MaxConcurrency = 40;
     private const int MinConcurrency = 1;
     
     private static readonly GraphicsDevice Gpu = GraphicsDevice.Default;
